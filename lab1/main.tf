35a4d96bc181b55de53cf679128a7b44aea7b290
Dries Dams <dries.dams@bryxx.eu>
2021-11-08 15:15:27 +0100

    initial commit

---
+++ lab1/main.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+  region = "us-east-1"
+}
+resource "aws_instance" "vm" {
+  ami           = "ami-01cc34ab2709337aa"
+  instance_type = "t3.micro"
+  tags = {
+    Name = "my-first-tf-node"
+  }
+}
