849b54cdb743426cc396160f86794d31f033dfbf
Dries Dams <dries.dams@bryxx.eu>
2021-11-09 01:11:35 +0100

    add lab4 resources

---
+++ lab4/modules/vpc/main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_vpc" "this" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "this" {
+  vpc_id     = aws_vpc.this.id
+  cidr_block = "10.0.1.0/24"
+}
---
+++ lab4/modules/vpc/outputs.tf
@@ -0,0 +1,7 @@
+output "subnet_id" {
+  value = aws_subnet.this.id
+}
+
+output "ami_id" {
+  value = "ami-01cc34ab2709337aa"
+}
