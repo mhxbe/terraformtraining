b53965d0f5e4dd917570a9476b24c083ffbfd4fc
Mike Henderyckx <mike.henderyckx@gmail.com>
2022-03-31 15:27:51 +0200

    lab4 changes

--- lab4/main.tf
+++ lab4/main.tf
@@ -16,4 +16,7 @@ resource "aws_instance" "my-instance" {
   ami           = module.vpc.ami_id
   subnet_id     = module.vpc.subnet_id
   instance_type = "t2.micro"
+  tags = {
+    Name = "vm-module-mike"
+  }
 }
